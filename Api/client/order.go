e8f38f48848139ca9797654837b8b7d2054eed31
Yooz <13212341544@163.com>
2025-06-14 12:00:01 +0800

    feat: 添加支付宝集成、车辆品牌管理和测试文件

---
+++ Api/client/order.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	order "order_srv/proto_order"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+// OrderClient 订单服务客户端
+func OrderClient(ctx context.Context, call func(context.Context, order.OrderClient) (interface{}, error)) (interface{}, error) {
+	// 连接订单服务
+	conn, err := grpc.Dial("localhost:9093", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("连接订单服务失败: %v", err)
+	}
+	defer conn.Close()
+
+	// 创建客户端
+	client := order.NewOrderClient(conn)
+
+	// 调用服务
+	return call(ctx, client)
+}
---
+++ Api/handler/order.go
@@ -0,0 +1,68 @@
+package handler
+
+import (
+	"context"
+
+	"Api/client"
+	order "order_srv/proto_order"
+)
+
+// CreateOrderFromReservation 基于预订创建订单
+func CreateOrderFromReservation(ctx context.Context, req *order.CreateOrderFromReservationRequest) (*order.CreateOrderFromReservationResponse, error) {
+	orderClient, err := client.OrderClient(ctx, func(ctx context.Context, in order.OrderClient) (interface{}, error) {
+		response, err := in.CreateOrderFromReservation(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+		return response, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return orderClient.(*order.CreateOrderFromReservationResponse), nil
+}
+
+// GetOrder 获取订单详情
+func GetOrder(ctx context.Context, req *order.GetOrderRequest) (*order.GetOrderResponse, error) {
+	orderClient, err := client.OrderClient(ctx, func(ctx context.Context, in order.OrderClient) (interface{}, error) {
+		response, err := in.GetOrder(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+		return response, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return orderClient.(*order.GetOrderResponse), nil
+}
+
+// UpdateOrderStatus 更新订单状态
+func UpdateOrderStatus(ctx context.Context, req *order.UpdateOrderStatusRequest) (*order.UpdateOrderStatusResponse, error) {
+	orderClient, err := client.OrderClient(ctx, func(ctx context.Context, in order.OrderClient) (interface{}, error) {
+		response, err := in.UpdateOrderStatus(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+		return response, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return orderClient.(*order.UpdateOrderStatusResponse), nil
+}
+
+// AlipayNotify 支付宝异步通知
+func AlipayNotify(ctx context.Context, req *order.AlipayNotifyRequest) (*order.AlipayNotifyResponse, error) {
+	orderClient, err := client.OrderClient(ctx, func(ctx context.Context, in order.OrderClient) (interface{}, error) {
+		response, err := in.AlipayNotify(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+		return response, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return orderClient.(*order.AlipayNotifyResponse), nil
+}
